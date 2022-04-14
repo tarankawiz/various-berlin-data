052ea6268b78d96a5826390fcb864b5c579848be
Kateryna Taranets <kateryna.taranets.90+github@gmail.com>
2022-04-14 20:55:22 +0200

    added new changes

--- lessons/lesson_001/aggregate_and_orderby_practice.sql
+++ lessons/lesson_001/aggregate_and_orderby_practice.sql
@@ -1,10 +1,14 @@
 select count(distinct "Ortst-Name")
 from "EWR_Ortsteile_2020-12-31";
 
+
 select count(*)
 from "EWR_Ortsteile_2020-12-31";
 
 select "Ortsteil"
+from "EWR_Ortsteile_2020-12-31";
+
+select "Bezirk"
 from "EWR_Ortsteile_2020-12-31";
 
 select sum("Ortsteil") as sum_of_ids
@@ -18,3 +22,89 @@ select distinct "Ortsteil"
 from "EWR_Ortsteile_2020-12-31"
 order by "Ortsteil" asc
 limit 1;
+
+select *
+from "EWR_Ortsteile_2020-12-31"
+where "Häufigkeit" between 100 and 200;
+
+select "Häufigkeit"
+from "EWR_Ortsteile_2020-12-31"
+order by "Häufigkeit" desc;
+
+
+CREATE TABLE test
+(
+    id         INT,
+    title      VARCHAR(30),
+    release    INT,
+    actor      VARCHAR,
+    director   VARCHAR,
+    box_office DECIMAL(5, 1)
+);
+
+select *
+from test;
+
+
+SELECT *
+FROM "EWR_Ortsteile_2020-12-31";
+
+alter table "EWR_Ortsteile_2020-12-31"
+    add column number varchar;
+
+select number
+from "EWR_Ortsteile_2020-12-31";
+
+INSERT INTO "EWR_Ortsteile_2020-12-31" ("Bezirk")
+values (13);
+
+select *
+from "EWR_Ortsteile_2020-12-31";
+
+select *
+from "EWR_Ortsteile_2020-12-31"
+order by "Bezirk" desc;
+
+delete
+from "EWR_Ortsteile_2020-12-31"
+where "Bezirk" = 13
+   or "Bezirk" is null;
+
+
+
+INSERT INTO test("box_office")
+values (6),
+       (7),
+       (8),
+       (12),
+       (17),
+       (25);
+
+select *
+from test;
+
+INSERT INTO "EWR_Ortsteile_2020-12-31" ("number")
+values (3);
+
+select *
+from "EWR_Ortsteile_2020-12-31";
+
+select "number"
+from "EWR_Ortsteile_2020-12-31";
+
+select distinct "number"
+from "EWR_Ortsteile_2020-12-31";
+
+select distinct "Bezirk"
+from "EWR_Ortsteile_2020-12-31";
+
+delete
+from "EWR_Ortsteile_2020-12-31"
+where "Bezirk" in (6);
+
+select "Bezirk"
+from "EWR_Ortsteile_2020-12-31";
+
+select *
+from test;
+
--- lessons/lesson_001/groupby_practice.sql
+++ lessons/lesson_001/groupby_practice.sql
@@ -1,4 +1,22 @@
 -- How many people are in each district?
 -- 3769962 altogether
 select sum("Häufigkeit") as amount_of_people
+from "EWR_Ortsteile_2020-12-31";
+
+select "Bez-Name"
+from "EWR_Ortsteile_2020-12-31";
+
+select distinct "Bez-Name"
+from "EWR_Ortsteile_2020-12-31";
+
+-- all people of berlin
+select sum("Häufigkeit") as amountOfPeople
+from "EWR_Ortsteile_2020-12-31";
+
+-- by district
+select "Bez-Name",
+       count(*)          as number_of_entries,
+       sum("Häufigkeit") as amountOfPeople
 from "EWR_Ortsteile_2020-12-31"
+group by "Bez-Name"
+order by number_of_entries
